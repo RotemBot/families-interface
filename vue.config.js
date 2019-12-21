f4c3f8d3d59417db135049be47762c53d20ec2ff
rotembo <rotem@dataloop.ai>
2019-12-22 00:08:33 +0200

    updated tslint and hot reload

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  pluginOptions: {
+    quasar: {
+      importStrategy: 'manual',
+      rtlSupport: true
+    }
+  },
+  transpileDependencies: [
+    'quasar'
+  ],
+  runtimeCompiler: true,
+  lintOnSave: false
+}
